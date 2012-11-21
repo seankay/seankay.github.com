38256847920e57e90f4c167d45c11fcb2b4eb6c5
Sean Kay <f.sean.kay@gmail.com>
2012-11-20 20:32:55 -0500

    Fill out about page

--- js/custom.js
+++ js/custom.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
 	$('#home').click(function() { loadPage('home') } );
 	$('#projects').click(function() { loadPage('projects') } );
 	$('#about').click(function() { loadPage('about') } );
+	initPopover('#nav-skype');
 });
 
-function email(name, domain, withlink) {
+function email(name, domain, withImage, msg) {
     var addr = name + '@' + domain;
-    if(withlink) {
-        document.write('<a href="mailto:' + addr + '"><img src="img/mail.png" alt="email height="32px" width="32px"></a>');
+    if(withImage) {
+        document.write('<a id="nav-email" href="mailto:' + addr + '"><img src="img/mail.png" alt="email height="32px" width="32px"></a>');
     } else {
-        return addr;
+        document.write('<a id="nav-email" href="mailto:' + addr + '">' + msg +'</a>');
     }
 }
 
@@ -21,4 +22,49 @@ function loadPage(page) {
 	else
 		page = page + '.html';
 	$('#content').load('/' + page + ' #content');
+}
+
+function proxyClick(id)
+{
+	id = '#' + id;
+	switch(id) {
+		case '#nav-email':
+		case '#nav-skype':
+			$(id)[0].click();
+			break;
+		default:
+			console.log("Unkown ID:" + id);
+	}
+}
+
+function initPopover(id)
+{
+	$(id).popover({
+		animation: true,
+		html: true,
+		placement: 'bottom',
+		trigger: 'click',
+		title: initPopoverTitle(id),
+		content: initPopoverContent(id)
+	})
+}
+
+function initPopoverContent(id)
+{
+	switch(id) {
+		case '#nav-skype':
+			return "<div class='well'><p class='tk-p-font'>dotxep</p></div>"
+		default:
+			console.log("Unknown ID:" + id);
+	}
+}
+
+function initPopoverTitle(id)
+{
+	switch(id) {
+		case '#nav-skype':
+			return "<div><p class='tk-p-font text-info'>Skype Username:</p></div>"
+		default:
+			console.log("Unknown ID:" + id);
+	}
 }
