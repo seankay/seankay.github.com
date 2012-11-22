fc844a45db0fe06891157694e0944798489925bb
Sean Kay <f.sean.kay@gmail.com>
2012-11-22 16:23:01 -0500

    Add employment button and popup notification

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,123 @@
+var employment_status = "employed";
+var social = ['twitter', 'github', 'linkedin','rss'];
+
 $(document).ready(function() {
-	$('#home a').attr('href', '/')
-	$('#projects a').attr('href', 'projects.html')
-	$('#about a').attr('href', 'about.html')
-	initPopover('#nav-skype');
+	$('#home a').attr('href', '/');
+	$('#projects a').attr('href', 'projects.html');
+	initSocialBarIcons(social);
+	createEmploymentButton(employment_status);
+	$('#social-email').append(email('f.sean.kay','gmail.com',true, true));
 });
 
-function email(name, domain, withImage, msg) {
+function initSocialBarIcons(social) {
+	var socialId = "";
+	for( var i = 0; i < social.length; i++) {
+		socialId = "#social-" + social[i] + " a";
+		$(socialId).append(getImage(social[i]));
+	}
+}
+
+/* 
+ * Creates the employment button on the projects page.
+ * Button created depends on employment_status.
+ * 'employed' : creates red button
+ * 'unemployed' : creates green button
+ */
+function createEmploymentButton(status) {
+	var btn = "";
+	switch(status) {
+		case 'employed':
+			btn = "<a id='employmentBtn' class='btn btn-red btn-small tk-p-font'>Currently Employed!</a>"
+			break;
+		case 'unemployed':
+			btn = "<a id='employmentBtn' class='btn btn-green btn-small tk-p-font' href='mailto:"
+			 + email('f.sean.kay', 'gmail.com', false, false) + "'>Contact me!</a>"
+			break;
+		default: console.log("No idea what that status means. Maybe you should have some coffee ...");
+	}
+
+	$('#project-header').append(btn);
+
+	//Bind action to button
+	$('#employmentBtn').click(function() { evaluateEmploymentStatus(status) } );
+}
+
+function evaluateEmploymentStatus(status) {
+	var msg = "";
+	var popupColor = "";
+	switch(status) {
+		case('employed'):
+			msg = "<h4>Sorry!</h4><p class='tk-p-font'>I'm currently not taking on any more projects. Feel free to leave me an email!</p>";
+			popupColor = "red";
+			break;
+		case('unemployed'):
+			msg = "<h4>Thanks!</h4><p class='tk-p-font'>I'll get back to you as soon as humanly possible.</p>";
+			popupColor = "green";
+			break;
+		default: console.log("No idea what that status means. Maybe you should have some coffee ...");
+	}
+	var popup = initPopup('popup',popupColor, msg)
+	displayPopup(popup);
+}
+
+function displayPopup(popup) {
+	popup.slideDown('slow');
+}
+
+function initPopup(id, popupColor, msg) {
+	var alertClass = "";
+	var popup = $('#' + id);
+	switch(popupColor) {
+		case 'red':
+			alertClass = "alert-error";
+			break;
+		case 'green':
+			alertClass = "alert-success";
+			break;
+		default: 
+			alertClass = "alert-info";
+	}
+	popup.empty();
+	popup.append("<div class='alert alert-block " + alertClass + "'>\
+		  <button type='button' class='close' data-dismiss='alert'>x</button>" + msg +
+		"</div>").hide();
+
+	return popup;
+}
+
+function email(name, domain, withImage, withLink) {
     var addr = name + '@' + domain;
-    if(withImage) {
-        document.write('<a id="nav-email" href="mailto:' + addr + '"><img src="/img/mail.png" alt="email height="32px" width="32px"></a>');
-    } else {
-        document.write('<a id="nav-email" href="mailto:' + addr + '">' + msg +'</a>');
+
+    if(withLink)
+    	if(withImage) {
+    		return ('<a href="mailto:' + addr + '">'+ getImage('email') + '</a>');	
+    	} else {
+        	return ('<a href="mailto:' + addr + '">' + addr + '</a>');
     }
+    else
+    	return addr;
+}
+
+function getImage(image)
+{
+	switch(image){
+		case 'email':
+			return "<img src='/img/mail.png' alt='email height='32px' width='32px'>";
+			break;
+		case 'twitter':
+			return "<img src='/img/twitter.png' alt='twitter' height='32px' width='32px'>";
+			break;
+		case 'linkedin':
+			return "<img src='/img/linkedin.png' alt='linkedin' height='32px' width='32px'>";
+			break;
+		case 'rss':
+			return "<img src='/img/rss.png' alt='rss' height='32px' width='32px'>";
+			break;
+		case 'github':
+			return "<img src='/img/octocat.png' alt='github' height='32px' width='32px'>";
+			break;
+		default: console.log("I don't know what image that is.");
+	}
 }
 
 function loadPage(page) {
